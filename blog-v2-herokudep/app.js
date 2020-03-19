e4ab5a3726c06b7206096e658352e165a4658836
Joseph Kligel <jkligel@gmail.com>
2020-03-19 01:08:03 -0500

    Using bash script to push all updates

--- blog-v2-herokudep/app.js
+++ blog-v2-herokudep/app.js
@@ -11,13 +11,14 @@ const aboutContent = "This is a blog site where I share my ideas and thoughts wi
 const contactContent = "You can get in touch with me via ";
 
 const app = express();
+let hiddenTitle = "";
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static("public/"));
 
 mongoose.connect("mongodb+srv://jkligel:1618@cluster0-ax2hg.mongodb.net/blogDB", {
   useNewUrlParser: true,
@@ -30,6 +31,8 @@ const postSchema = {
 };
 
 const Post = mongoose.model("Post", postSchema);
+
+// --------------Gets Section--------------
 
 app.get("/", function(req, res) {
   Post.find({}, function(err, posts) {
@@ -57,20 +60,48 @@ app.get("/contact", function(req, res) {
 });
 
 app.get("/compose", function(req, res) {
-  res.render("compose");
+  const hiddenTitle = req.query.title;
+  res.render("compose", {
+    journalTitle: hiddenTitle
+  });
 });
 
-app.post("/compose", function(req, res) {
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
+// --------------Posts Section--------------
 
-  post.save(function(err) {
-    if (!err) {
-      res.redirect("/");
-    }
-  });
+app.post("/compose/:title", function(req, res) {
+  const title = _.capitalize(req.params.title);
+
+  if (title === "Daily") {
+    const post = new Post({
+      title: req.body.postTitle,
+      content: req.body.postBody
+    });
+
+    post.save(function(err) {
+      if (!err) {
+        res.redirect("/");
+      }
+    });
+  } else {
+    const title = _.lowerCase(req.params.title);
+    mongoose.connection.db.listCollections({
+      name: title
+    }).next(
+      function(err, collinfo) {
+        if (collinfo) {
+          const journalPost = new Journal({
+            title: req.body.postTitle,
+            content: req.body.postBody
+          });
+
+          journalPost.save(function(err) {
+            if (!err) res.redirect("/" + title);
+          });
+        }
+      }
+    );
+  }
+
 });
 
 app.get("/posts/:postId", function(req, res) {
@@ -89,41 +120,79 @@ app.get("/posts/:postId", function(req, res) {
   });
 });
 
-// ---------------New Journal Section --------------
+// ---------------Other Journal Section --------------
 
-const journalSchema = {
-  journalName: String,
-  posts: [postSchema]
-};
-
-const Journal = mongoose.model("Journal", journalSchema);
 
 app.get("/:journal", function(req, res) {
   const requestedTitle = _.capitalize(req.params.journal);
 
-  Journal.findOne({
-      journalName: requestedTitle
-    }, function(err, foundJournal) {
-      if (err) console.log(err);
-      else if (!foundJournal) {
-        const journal = new Journal({
-          journalName: requestedTitle
-        });
-        journal.save();
-        res.redirect("/" + requestedTitle);
-      } else {
-        res.render("journal", {
-          journalTitle: foundJournal.journalName,
-          startingContent: "This is the " + foundJournal.journalName + " Journal.",
-          posts: foundJournal.posts
-        });
-      }
-    });
+  mongoose.connection.db.listCollections({
+    name: _.lowerCase(req.params.journal)
+  }).next(function(err, collinfo) {
+    if (collinfo) {
 
+      try { //Don't know how but the next 5 lines work
+        Journal = mongoose.connection.model(collinfo.name)
+      } catch (e) {
+        Journal = mongoose.model(collinfo.name, postSchema)
+      }
+      module.exports = Journal;
+
+      Journal.find({}, function(err, foundPosts) {
+        if (err) console.log(err);
+        else {
+          res.render("journal", {
+            journalTitle:  requestedTitle,
+            headerTitle: header(requestedTitle),
+            startingContent: "This is the " + requestedTitle.slice(0,-5) + " progess log",
+            posts: foundPosts
+          });
+        }
+      });
+    } else res.sendFile(__dirname + "/public/error/404.html");
   });
+});
+
+app.get("/:journal/:postId", function(req, res) {
+  const requestedJournal = req.params.journal;
+  const requestedId = req.params.postId;
+
+  mongoose.connection.db.listCollections({
+    name: _.lowerCase(requestedJournal)
+  }).next(function(err, collinfo) {
+    if (collinfo) {
+      Journal.findOne({
+        _id: requestedId
+      }, function(err, foundPost) {
+        if (err) console.log(err);
+        else {
+          res.render("post", {
+            title: foundPost.title,
+            content: foundPost.content
+          });
+        }
+      });
+    } else res.sendFile(__dirname + "/public/error/404.html");
+  });
+});
 
 // ---------------Port Section --------------
+
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
+
+// ---------------Miscallaneous Section --------------
+
+function header(string){
+  var header = string.slice(0, -5);
+  if(header === "Py"){
+    header = "Python Programming";
+  } else if(header === "Wd") {
+    header = "Web Development (HTML, CSS, JS, NodeJS)";
+  } else {
+    header = header + " Programming";
+  }
+  return header;
+};
